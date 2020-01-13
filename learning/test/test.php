4043411dad253e5e19bdc8999e32ed7c37d4d9f2
Ubuntu <nadav@ubuntu.dvxdt2r1uhie5lcvylycbs4hpf.ax.internal.cloudapp.net>
2020-01-13 17:44:30 +0000

    test container for php

---
+++ learning/test/test.php
@@ -0,0 +1,18 @@
+<html>
+ <head>
+  <title>I'm just testing stuff</title>
+ </head>
+ <body>
+<body bgcolor="#f1f1f1" text="Blue">
+<img src="http://4.bp.blogspot.com/-JQwen3M09tw/VpTFbGHVgMI/AAAAAAAAAFI/m7sRDGpBdCs/s1600/DckerStart.jpeg" alt="Docker">
+<h1>If you're seeing this, your website works!</h1>
+<?php
+echo 'Your IP Address is ';
+echo $_SERVER['REMOTE_ADDR'];
+echo "<br>";
+echo 'Your browser is ';
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+?> 
+ </body>
+</html>
