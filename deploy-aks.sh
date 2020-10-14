89c6edfb9181646e7e0e4774ba25552dfe954d28
Nadav Ben Haim [Microsoft] <29725442+msftnadavbh@users.noreply.github.com>
2020-10-14 15:14:28 +0300

    Update deploy-aks.sh

--- deploy-aks.sh
+++ deploy-aks.sh
@@ -3,12 +3,22 @@
 RESOURCEGROUP=rg-aks-neu
 LOCATION=northeurope
 AKS=aks-neu
-VNETname=vnet-aks-neu
+VNET=vnet-aks-neu
 
 
 az group create -l $LOCATION -n $RESOURCEGROUP
 
 az network vnet create \
 --resource-group $RESOURCEGROUP \
---name $VNETname \
+--name $VNET \
 --address-prefixes 172.16.0.0/16
+
+az network vnet subnet create \
+--resource-group $RESOURCEGROUP \
+--vnet-name $VNET \
+--name aks-vnet-subnet \
+--address-prefixes 172.16.0.0/16 \
+--service-endpoints Microsoft.ContainerRegistry
+
+
+SUBNET_ID=$(az network vnet subnet show -n aks-vnet-subnet -g $RESOURCEGROUP --vnet-name $VNET --query id)
