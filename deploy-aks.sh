5c3d1767e9c3ab549920c263ad3cdc9d887cc107
Nadav Ben Haim [Microsoft] <29725442+msftnadavbh@users.noreply.github.com>
2020-10-14 15:04:07 +0300

    Update deploy-aks.sh

--- deploy-aks.sh
+++ deploy-aks.sh
@@ -3,8 +3,12 @@
 RESOURCEGROUP=rg-aks-neu
 LOCATION=northeurope
 AKS=aks-neu
+VNETname=vnet-aks-neu
 
 
 az group create -l $LOCATION -n $RESOURCEGROUP
 
-az network
+az network vnet create \
+--resource-group $RESOURCEGROUP \
+--name $VNETname \
+--address-prefixes 172.16.0.0/16
