a62801a6366192d4aa0af224d7490a54cd37eb64
Nadav Ben Haim [Microsoft] <29725442+msftnadavbh@users.noreply.github.com>
2020-10-14 15:16:44 +0300

    Update deploy-aks.sh

--- deploy-aks.sh
+++ deploy-aks.sh
@@ -22,3 +22,17 @@ az network vnet subnet create \
 
 
 SUBNET_ID=$(az network vnet subnet show -n aks-vnet-subnet -g $RESOURCEGROUP --vnet-name $VNET --query id)
+
+az aks create \
+    --resource-group $RESOURCEGROUP \
+    --name $AKS \
+    --load-balancer-sku standard \
+    --enable-private-cluster \
+    --network-plugin azure \
+    --vnet-subnet-id $SUBNET_ID \
+    --node-count 2 \
+    --kubernetes-version 1.17.9 \
+    --docker-bridge-address 172.17.0.1/16 \
+    --dns-service-ip 10.2.0.10 \
+    --service-cidr 10.2.0.0/24 \
+    --no-wait
