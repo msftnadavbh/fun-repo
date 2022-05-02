3542c0aea778122f7967347e0e459ac3358977d6
msftnadavbh <nadavbh@microsoft.com>
2022-05-02 09:31:29 +0300

    new folder + files

---
+++ test4fun/test.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>Test Website</title>
+ </head>
+ <body>
+<body bgcolor="#f1f1f1" text="Blue">
+<h1>I'm just a test webpage</h1>
+<?php
+echo "<br>";
+echo 'Congratz, your website is working! ';
+echo "<br>";
+
+?> 
+ </body>
+</html>
