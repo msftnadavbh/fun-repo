08529c3f61b2a486d68b0c4c321420e1a8ff3e0a
msftnadavbh <nadavbh@microsoft.com>
2022-05-02 09:51:22 +0300

    fun

--- test4fun/test.php
+++ test4fun/test.php
@@ -5,6 +5,7 @@
  <body>
 <body bgcolor="#f1f1f1" text="Blue">
 <h1>I'm just a test webpage</h1>
+<img src="https://funstorage90.blob.core.windows.net/images/test.png" alt="Logo of a company" />
 <?php
 echo "<br>";
 echo 'Congratz, your website is working! ';
